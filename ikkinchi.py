c3494787439ec0cb35925e4b5cc1ebecc55329b6
newbiestart <ptmaevim@gmail.com>
2025-07-28 14:43:39 +0500

    ozgartirish

--- ikkinchi.py
+++ ikkinchi.py
@@ -1,4 +1,41 @@
-def this():
-    for i in range(20):
-        print(i)
-this()
+# def this():
+#     for i in range(20):
+#         print(i)
+# this()
+
+# def my_function(child3, child2, child1):
+#   print("The youngest child is " + child3)
+
+# my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+
+
+# def my_function(**kid):
+#   print("His last name is " + kid["lname"])
+
+# my_function(fname = "Tobias", lname = "Refsnes")
+
+# def my_function(country = "Norway"):
+#   print("I am from " + country)
+
+# my_function("Sweden")
+# my_function("India")
+# my_function()
+# my_function("Brazil")
+
+
+
+# def my_function(food):
+#   for x in food:
+#     print(x)
+
+# fruits = ["apple", "banana", "cherry"]
+
+# my_function(fruits)
+
+
+def my_function(x):
+  return 5 * x
+
+print(my_function(3))
+print(my_function(5))
+print(my_function(9))
